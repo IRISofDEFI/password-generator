35b1e5e90f3013e74d04173f80f9c711cd5d6d5c
IRIS <irisofdefi@gmail.com>
2025-09-24 18:52:36 +0100

    building an password generator

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-const minIncomeForDuplex = 60000;
-const minCreditScoreForDuplex = 700;
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=[]{}\\|;':\",./?<>~`";
 
-const minIncomeForCondo = 45000;
-const minCreditScoreForCondo = 680;
+const lengthEl = document.getElementById("length");
 
-const minIncomeForCar = 30000;
-const minCreditScoreForCar = 650;
+const lowercaseEl = document.getElementById("lowercase");
 
-function getLoanMessage(annualIncome, creditScore) {
-    if (creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
-        return "You qualify for a duplex, condo, and car loan."
-    } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
-        return "You qualify for a condo and car loan."
-    } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
-        return "You qualify for a car loan."
-    } else {
-        return "You don't qualify for any loans."
+const uppercaseEl = document.getElementById("uppercase");
+
+const numbersEl = document.getElementById("numbers");
+
+const symbolsEl = document.getElementById("symbols");
+
+const generateBtn = document.getElementById("generate");
+
+const passwordEl = document.getElementById("password");
+
+generateBtn.addEventListener("click", function () {
+
+    const length = lengthEl.value;
+    let characters = "";
+    let password = "";
+
+    if (lowercaseEl.checked) {
+        characters += lowercaseLetters;
     }
-}
 
-const duplexLoanMsg = getLoanMessage(85000, 850);
-const condoLoanMsg = getLoanMessage(65000, 690);
-const carLoanMsg = getLoanMessage(45000, 660);
-const noLoanMsg = getLoanMessage(25000, 550);
+    if (uppercaseEl.checked) {
+        characters += uppercaseLetters;
+    }
 
-console.log(duplexLoanMsg);
-console.log(condoLoanMsg);
-console.log(carLoanMsg);
-console.log(noLoanMsg);
+    if (numbersEl.checked) {
+        characters += numbers;
+    }
+
+    if (symbolsEl.checked) {
+        characters += symbols;
+    }
+
+    if (characters === "") {
+        alert("Please select at least one character type!");
+        return;
+    }
+
+    for (i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length))
+
+    }
+    passwordEl.value = password;
+
+});
 
 
-// Learn about grocery shopping lists
-console.log("Grocery shopping list");
+const copyBtn = document.getElementById("copy");
 
-const shoppingList = [];
+copyBtn.addEventListener("click", function () {
+    const password = passwordEl.value;
 
-console.log("It will be nice to have some fruit to eat.");
+    if (!password) {
+        alert("No password to copy!");
+        return;
+    }
 
-shoppingList.push("Apples");
-
-function getShoppingListMsg(arr) {
-    return `Current Shopping List: ${arr}`;
-}
-
-console.log(getShoppingListMsg(shoppingList));
-
-shoppingList.push("Grapes");
-console.log(getShoppingListMsg(shoppingList));
-
-console.log("It looks like we need to get some cooking oil.");
-
-shoppingList.unshift("Vegetable Oil");
-console.log(getShoppingListMsg(shoppingList));
-
-shoppingList.push("Popcorn", "Beef Jerky", "Potato Chips");
-console.log(getShoppingListMsg(shoppingList));
-
-console.log("This looks like too much junk food.");
-
-shoppingList.pop();
-console.log(getShoppingListMsg(shoppingList));
-
-console.log("It might be nice to get a dessert.");
-
-shoppingList.unshift("Chocolate Cake");
-console.log(getShoppingListMsg(shoppingList));
-
-console.log("On second thought, maybe we should be more health conscious.");
-
-shoppingList.shift();
-shoppingList[0] = "Canola Oil";
+    // Copy to clipboard
+    navigator.clipboard.writeText(password).then(() => {
+        alert("Password copied to clipboard! ✅");
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
+});
